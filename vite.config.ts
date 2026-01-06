972d12d68a400bcad8b80652169c4d005c571950
dunren-cmd <dunren02@dunren.com.tw>
2026-01-06 09:09:26 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "node:path"
 
 export default defineConfig({
-  base: "/obgit/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
+
 
